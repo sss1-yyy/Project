940bfbbff41065e8362f86fb454ec7fc7e924f93
sss1-yyy <ksui9210@gmail.com>
2026-09-15 15:57:49 +0900

    fix : 원상복구

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MovieLogApp());
+  runApp(const MyApp());
 }
 
-class MovieLogApp extends StatelessWidget {
-  const MovieLogApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'MovieLog',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        useMaterial3: true,
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const StartScreen(),
-    );
-  }
-}
-
-class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
-  const Text(
-  '영화의 순간을 기록하세요',
-  textAlign: TextAlign.center,
-  maxLines: 1,
-  overflow: TextOverflow.ellipsis,
-  style: TextStyle(
-    fontSize: 24,
-    fontWeight: FontWeight.bold,
-  ),
-);
-const Icon(
-  Icons.movie_outlined,
-  size: 72,
-  color: Colors.deepPurple,
-);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Icon(Icons.movie_outlined),
-            Text('MovieLog'),
-          ],
-        ),
-      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
